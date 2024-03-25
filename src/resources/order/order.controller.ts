e2b1a625ae7c417b19a5b03a6ed171d024c6c201
micheal081 <michig3000@gmail.com>
2024-03-25 02:01:14 +0100

    feat: complete api endpoints

--- src/resources/order/order.controller.ts
+++ src/resources/order/order.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Get, UseGuards, Param, Query } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Order } from '@prisma/postgres/client';
@@ -18,5 +18,43 @@ export class OrderController {
   @Post('save')
   saveNewOrder(@AuthUser() user: UserContext, @Body() payload: NewOrderDto): Promise<Partial<Order>> {
     return this.orderService.saveOrder(user.id, payload);
+  }
+
+  @Get(':id')
+  getOrderById(@AuthUser() user: UserContext, @Param('id') id: string): Promise<Partial<Order>> {
+    return this.orderService.getOrderById(id, user.id);
+  }
+
+  @Get('detailsForToday')
+  getOrderDetailsForToday(
+    @AuthUser() user: UserContext,
+    @Query('offset') offset: number,
+    @Query('limit') limit: number,
+  ): Promise<Partial<Order>[]> {
+    return this.orderService.getOrderDetailsForToday(user.id, offset, limit);
+  }
+
+  @Get('overallDetails')
+  getOverallOrderDetails(
+    @AuthUser() user: UserContext,
+    @Query('offset') offset: number,
+    @Query('limit') limit: number,
+  ): Promise<Partial<Order>[]> {
+    return this.orderService.getOverallOrderDetails(user.id, offset, limit);
+  }
+
+  @Get('totalForToday')
+  getTotalNumberForToday(@AuthUser() user: UserContext): Promise<number> {
+    return this.orderService.getTotalNumberForToday(user.id);
+  }
+
+  @Get('overallNumber')
+  getOverallNumber(@AuthUser() user: UserContext): Promise<number> {
+    return this.orderService.getOverallNumber(user.id);
+  }
+
+  @Get('creditScore')
+  getCreditScore(@AuthUser() user: UserContext): Promise<number> {
+    return this.orderService.calculateCreditScore(user.id);
   }
 }
